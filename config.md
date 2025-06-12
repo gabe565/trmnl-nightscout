33544ef4823a5de2360845bbc942989a8fb5db98
Gabe Cook <gabe565@gmail.com>
2025-06-12 16:11:07 -0500

    docs(config): change config order

--- config.md
+++ config.md
@@ -3,21 +3,21 @@
 ## Config
 
  - `LISTEN_ADDRESS` (**required**, non-empty, default: `:8080`) - HTTP server bind address.
- - `REAL_IP_HEADER` (default: `false`) - Get client IP address from the "Real-IP" header.
- - `ACCESS_TOKEN` - Token required to access the API. If set, the value must be provided as a `token` query parameter.
  - `PUBLIC_URL` (**required**, non-empty) - This app's public URL.
+ - `ACCESS_TOKEN` - Token required to access the API. If set, the value must be provided as a `token` query parameter.
+ - `REAL_IP_HEADER` (default: `false`) - Get client IP address from the "Real-IP" header.
  - `NIGHTSCOUT_URL` (**required**, non-empty) - Nightscout base URL
  - `NIGHTSCOUT_TOKEN` - Nightscout token. Using an access token is recommended instead of the API secret.
  - `NIGHTSCOUT_UNITS` - Blood sugar unit. (one of: mg/dL, mmol/L)
- - `FETCH_DELAY` (default: `30s`) - Time to wait before the next reading should be ready. In testing, this seems to be about 20s behind, so the default is 30s to be safe. Your results may vary.
- - `FALLBACK_INTERVAL` (default: `30s`) - Normally, readings will be fetched when ready (after ~5m). This interval will be used if the next reading time cannot be estimated due to sensor warm-up, missed readings, errors, etc.
  - `TIME_FORMAT` (default: `3:04 PM`) - Customize the time format. Use `3:04 PM` for 12-hour time or `15:04` for 24-hour. See [time](https://pkg.go.dev/time) for more details.
  - `GRAPH_DURATION` (default: `6h`) - How far back in time the graph should go.
  - `HIGH_THRESHOLD` (default: `200`) - Where to draw the upper line.
- - `HIGH_BACKGROUND_SHADE` (default: `250`) - Background color above the high threshold line. Value must be between 0-255.
+ - `HIGH_BACKGROUND_SHADE` (default: `250`) - Background shade above the high threshold line. Value must be between 0-255.
  - `LOW_THRESHOLD` (default: `70`) - Where to draw the lower line.
- - `LOW_BACKGROUND_SHADE` (default: `247`) - Background color below the low threshold line. Value must be between 0-255.
+ - `LOW_BACKGROUND_SHADE` (default: `247`) - Background shade below the low threshold line. Value must be between 0-255.
  - `INVERT` - Render with a black background and a white foreground.
  - `INVERT_BELOW` (default: `55`) - Invert colors when below this value. (Stacks with the `INVERT` option)
  - `INVERT_ABOVE` (default: `300`) - Invert colors when above this value. (Stacks with the `INVERT` option)
+ - `FETCH_DELAY` (default: `30s`) - Time to wait before the next reading should be ready. In testing, this seems to be about 20s behind, so the default is 30s to be safe. Your results may vary.
+ - `FALLBACK_INTERVAL` (default: `30s`) - Normally, readings will be fetched when ready (after ~5m). This interval will be used if the next reading time cannot be estimated due to sensor warm-up, missed readings, errors, etc.
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,39 +8,45 @@ type Config struct {
 
 	// HTTP server bind address.
 	ListenAddress string `env:"LISTEN_ADDRESS,notEmpty" envDefault:":8080"`
-	// Get client IP address from the "Real-IP" header.
-	RealIPHeader bool `env:"REAL_IP_HEADER"          envDefault:"false"`
-	// Token required to access the API. If set, the value must be provided as a `token` query parameter.
-	AccessToken string `env:"ACCESS_TOKEN"`
 	// This app's public URL.
 	PublicURL string `env:"PUBLIC_URL,notEmpty"`
+	// Token required to access the API. If set, the value must be provided as a `token` query parameter.
+	AccessToken string `env:"ACCESS_TOKEN"`
+	// Get client IP address from the "Real-IP" header.
+	RealIPHeader bool `env:"REAL_IP_HEADER"          envDefault:"false"`
+
 	// Nightscout base URL
 	NightscoutURL string `env:"NIGHTSCOUT_URL,notEmpty"`
 	// Nightscout token. Using an access token is recommended instead of the API secret.
 	NightscoutToken string `env:"NIGHTSCOUT_TOKEN"`
+
 	// Blood sugar unit. (one of: mg/dL, mmol/L)
 	Units Unit `env:"NIGHTSCOUT_UNITS"`
-	// Time to wait before the next reading should be ready. In testing, this seems to be about 20s behind, so the default is 30s to be safe. Your results may vary.
-	FetchDelay time.Duration `env:"FETCH_DELAY"             envDefault:"30s"`
-	// Normally, readings will be fetched when ready (after ~5m). This interval will be used if the next reading time cannot be estimated due to sensor warm-up, missed readings, errors, etc.
-	FallbackInterval time.Duration `env:"FALLBACK_INTERVAL"       envDefault:"30s"`
 	// Customize the time format. Use `3:04 PM` for 12-hour time or `15:04` for 24-hour. See [time](https://pkg.go.dev/time) for more details.
-	TimeFormat string `env:"TIME_FORMAT"             envDefault:"3:04 PM"`
+	TimeFormat string `env:"TIME_FORMAT"      envDefault:"3:04 PM"`
 
 	// How far back in time the graph should go.
-	GraphDuration time.Duration `env:"GRAPH_DURATION"        envDefault:"6h"`
+	GraphDuration time.Duration `env:"GRAPH_DURATION" envDefault:"6h"`
+
 	// Where to draw the upper line.
 	HighThreshold float64 `env:"HIGH_THRESHOLD"        envDefault:"200"`
-	// Background color above the high threshold line. Value must be between 0-255.
+	// Background shade above the high threshold line. Value must be between 0-255.
 	HighBackgroundShade uint8 `env:"HIGH_BACKGROUND_SHADE" envDefault:"250"`
+
 	// Where to draw the lower line.
-	LowThreshold float64 `env:"LOW_THRESHOLD"         envDefault:"70"`
-	// Background color below the low threshold line. Value must be between 0-255.
-	LowBackgroundShade uint8 `env:"LOW_BACKGROUND_SHADE"  envDefault:"247"`
+	LowThreshold float64 `env:"LOW_THRESHOLD"        envDefault:"70"`
+	// Background shade below the low threshold line. Value must be between 0-255.
+	LowBackgroundShade uint8 `env:"LOW_BACKGROUND_SHADE" envDefault:"247"`
+
 	// Render with a black background and a white foreground.
 	Invert bool `env:"INVERT"`
 	// Invert colors when below this value. (Stacks with the `INVERT` option)
-	InvertBelow int `env:"INVERT_BELOW"          envDefault:"55"`
+	InvertBelow int `env:"INVERT_BELOW" envDefault:"55"`
 	// Invert colors when above this value. (Stacks with the `INVERT` option)
-	InvertAbove int `env:"INVERT_ABOVE"          envDefault:"300"`
+	InvertAbove int `env:"INVERT_ABOVE" envDefault:"300"`
+
+	// Time to wait before the next reading should be ready. In testing, this seems to be about 20s behind, so the default is 30s to be safe. Your results may vary.
+	FetchDelay time.Duration `env:"FETCH_DELAY"       envDefault:"30s"`
+	// Normally, readings will be fetched when ready (after ~5m). This interval will be used if the next reading time cannot be estimated due to sensor warm-up, missed readings, errors, etc.
+	FallbackInterval time.Duration `env:"FALLBACK_INTERVAL" envDefault:"30s"`
 }
