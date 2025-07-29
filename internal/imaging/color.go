38b34745441ae756ff67a468ad9bc8ebfdd275f6
Gabe Cook <gabe565@gmail.com>
2025-07-29 13:59:18 -0500

    feat(imaging): add vars for grays

--- internal/imaging/color.go
+++ internal/imaging/color.go
@@ -2,10 +2,16 @@ package imaging
 
 import "image/color"
 
+//nolint:gochecknoglobals
+var (
+	Gray1 = color.Gray{Y: 0x55}
+	Gray2 = color.Gray{Y: 0xAA}
+)
+
 func Palette1Bit() color.Palette {
 	return color.Palette{color.Black, color.White}
 }
 
 func Palette2Bit() color.Palette {
-	return color.Palette{color.Black, color.Gray{Y: 0x55}, color.Gray{Y: 0xAA}, color.White}
+	return color.Palette{color.Black, Gray1, Gray2, color.White}
 }
