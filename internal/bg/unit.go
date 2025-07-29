5c72eab2d317beb21e36d4d2bb0361b5673bdd82
Gabe Cook <gabe565@gmail.com>
2025-07-29 13:48:13 -0500

    fix(config): allow `mgdl` or `mmol` for unit config

--- internal/bg/unit.go
+++ internal/bg/unit.go
@@ -1,9 +1,11 @@
 //nolint:recvcheck
 package bg
 
+import "bytes"
+
 const MmolConversionFactor = 0.0555
 
-//go:generate go tool enumer -type Unit -linecomment -text
+//go:generate go tool enumer -type Unit -linecomment
 
 type Unit uint8
 
@@ -11,3 +13,26 @@ const (
 	Mgdl Unit = iota // mg/dL
 	Mmol             // mmol/L
 )
+
+// MarshalText implements the encoding.TextMarshaler interface for Unit.
+func (u Unit) MarshalText() ([]byte, error) {
+	return []byte(u.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface for Unit.
+func (u *Unit) UnmarshalText(text []byte) error {
+	var err error
+	if *u, err = UnitString(string(text)); err != nil {
+		switch string(bytes.ToLower(text)) {
+		case "mgdl":
+			*u = Mgdl
+			return nil
+		case "mmol":
+			*u = Mmol
+			return nil
+		default:
+			return err
+		}
+	}
+	return nil
+}
